cd196e2924e8161595055ee51c47411e2e5f3fcf
Shanova <shanova2602@gmail.com>
2025-08-04 15:50:42 +0530

    Initial commit

--- ecommerce/models.py
+++ ecommerce/models.py
@@ -34,3 +34,14 @@ class Order(models.Model):
 
     def __str__(self):
         return f"Order {self.id} by User {self.user.id}"
+
+
+class Customer(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
